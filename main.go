b697c0f2d85ec71d2f4738d92e3dbc653ac7bbdd
Gift__ <giftabumere247@gmail.com>
2023-01-07 11:40:26 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4562136268518-4570189977874-eypf0WPjjA2EqKgOM7NNlVPx")
+	os.Setenv("CHANNEL_ID", "C04GMP72VT7")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"file.txt"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s \n", file.Name, file.URL)
+	}
+}
